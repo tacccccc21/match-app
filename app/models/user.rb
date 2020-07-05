00d767118e36d3a7130d39ec4f9e29ff1d76c7c2
tac <tacccccc21@gmail.com>
2020-07-05 17:12:48 +0900

    create_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  has_many :chat_messages
+  has_many :chat_room_users
+  
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
 
-         mount_uploader :img_name, ImgNameUploader
+        mount_uploader :img_name, ImgNameUploader
 
         enum sex: { 男: 0, 女: 1 }
 end
